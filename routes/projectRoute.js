b97fcc23bbf6e2886cf9efe989bb15fadb4681a4
kyranmccann <kyran.mccann@gmail.com>
2018-11-09 09:07:37 -0800

    project routes set

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -5,8 +5,70 @@ const projectModel = require('../data/helpers/projectModel');
 router.get('/', (req, res) => {
   projectModel
     .get()
-    .then(posts=> res.status(200).json(posts))
+    .then(projects => res.status(200).json(projects))
     .catch(err => res.status(500).json({ error: "The project information could not be retrieved."}))
 })
 
-module.exports = router; 
+// router.get('/:id/actions', async (req, res) => {
+//   try {
+//     const actions = await projectModel.getProjectActions(req.params.id);
+//     actions
+//     ? res.status(200).json(actions)
+//     : res.status(404).json({ message: "Actions for that project number could not be found"})
+//   } catch (err) {
+//     res.status(500).json({ error: "The actions information could not be retrieved."})
+//   }
+// })
+
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await projectModel.get(req.params.id);
+    project
+      ? res.status(200).json(project)
+      : res.status(404).json({ message: "A project with that ID could not be found."})
+  }
+  catch (err) {
+    res.status(500).json({ error: "The project information could not be retrieved."})
+  }
+})
+
+router.post('/', async (req, res) => {
+  if (req.body.name && req.body.name.length < 129 && req.body.description){
+    try {
+      const newProject = await projectModel.insert(req.body);
+      const project = await projectModel.get(newProject.id);
+      res.status(201).json(project);
+    } catch (err) {
+      res.status(500).json({ error: "Could not add project."})
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await projectModel.remove(req.params.id);
+    count
+    ? res.status(200).json({ message: "The project was deleted.", id: `${req.params.id}`})
+    : res.status(404).json({ message: "A user with that ID could not be found."})
+  } catch (err) {
+    res.status(500).json({ error: 'Could not complete request.'})
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  if (req.body.name && req.body.name.length < 129 && req.body.description){
+    try {
+      const count = await projectModel.update(req.params.id, req.body);
+      if (count) {
+        const project = await projectModel.get(req.params.id);
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: "A project with that ID could not be found."})
+      }
+    } catch (err) {
+      res.status(500).json({ error: "The project could not be updated."})
+    }
+  }
+})
+
+module.exports = router;
