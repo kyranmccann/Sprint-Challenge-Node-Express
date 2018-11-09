a48de3efb73c97a031b8d2ff7e659424236fb194
kyranmccann <kyran.mccann@gmail.com>
2018-11-09 08:24:40 -0800

    setup server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('short'));
+server.use(cors());
+
+// const projectRoute = require('./routes/projectRoute');
+// const actionsRoute = require('./routes/actionRoute');
+//
+// server.use('/projects', projectRoute);
+// server.use('/actions', actionRoute);
+
+server.get('', (req, res) => {
+  res.json({ message: "the server is alive!"})
+})
+
+module.exports = server;
